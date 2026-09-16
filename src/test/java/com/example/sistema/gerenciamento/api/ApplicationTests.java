105b11dfefdfdabf14fd2672a28b86b3f2cfd250
JuliioCesar <juliotjs680@hotmail.com>
2026-09-16 14:26:41 -0300

    Initial commit

---
+++ src/test/java/com/example/sistema/gerenciamento/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistema.gerenciamento.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
